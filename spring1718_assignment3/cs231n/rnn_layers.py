9e961c2cdc4af1d473263ee1d9ae1a5d39351cc9
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-03-29 15:13:42 +0000

    implement word_embedding forward and backward

--- spring1718_assignment3/cs231n/rnn_layers.py
+++ spring1718_assignment3/cs231n/rnn_layers.py
@@ -198,7 +198,7 @@ def word_embedding_forward(x, W):
     #                                                                            #
     # HINT: This can be done in one line using NumPy's array indexing.           #
     ##############################################################################
-    pass
+    out, cache = W[x], (x, W)
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
@@ -227,7 +227,9 @@ def word_embedding_backward(dout, cache):
     # Note that words can appear more than once in a sequence.                   #
     # HINT: Look up the function np.add.at                                       #
     ##############################################################################
-    pass
+    (x, W) = cache
+    dW = np.zeros(W.shape)
+    np.add.at(dW, x, dout)
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
